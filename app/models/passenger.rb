11969db04ba100f09c48c7706a561559822cba47
Bernadette Davis <bsd231@nyu.edu>
2019-12-02 22:48:17 +0000

    Automatically backed up by Learn

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,9 @@
 class Passenger < ActiveRecord::Base
+<<<<<<< HEAD
   has_many :rides
   has_many :taxis, through: :rides
+=======
+  #Taxi -< Rides >- Passengers
+
+>>>>>>> 3db07c173b3d39b3da81f9936072d7b8a15ebffb
 end
